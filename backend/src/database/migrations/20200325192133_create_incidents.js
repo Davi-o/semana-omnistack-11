746cb6290bec87a3ba9039f6be63c065965dfd92
Davi-o <alvesdelima.davi@gmail.com>
2020-03-25 21:04:28 -0300

    Commit inicial

---
+++ backend/src/database/migrations/20200325192133_create_incidents.js
@@ -0,0 +1,17 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('incidents', function (table) {
+        table.increments(); // pk
+
+        table.string('title').notNullable();
+        table.string('description').notNullable();
+        table.string('value').notNullable();
+
+        table.string('ong_id').notNullable(); // fk
+
+        table.foreign('ong_id').references('id').inTable('ongs');
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('incidents');
+};
